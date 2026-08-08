ef3a50cf34810d27c0f51eae7d2956cae4ce9149
Sidney Cordeiro Santos <sidney270santos@gmail.com>
2026-08-07 23:04:41 -0300

    learning static members

--- JavaPooNotasEscola/src/entidades/Aluno.java
+++ JavaPooNotasEscola/src/entidades/Aluno.java
@@ -1,6 +1,10 @@
 package entidades;
 
 public class Aluno {
+
+	public static final double PESO_1_PERIODO = 0.30;
+	public static final double PESO_2_3_PERIODOS = 0.35;
+
 	public String nome;
 	public double[] notas = new double[3];
 
@@ -8,9 +12,9 @@ public class Aluno {
 		double media = 0;
 		for (int i = 0; i < notas.length; i++) {
 			if (i == 0) {
-				media = notas[i] * 0.30;
+				media = notas[i] * PESO_1_PERIODO;
 			} else {
-				media += notas[i] * 0.35;
+				media += notas[i] * PESO_2_3_PERIODOS;
 			}
 
 		}
@@ -20,8 +24,9 @@ public class Aluno {
 
 	public String resultadoFinal(double porcentagem) {
 		double resultado = media();
-		if (resultado < porcentagem/10) {
-			return "nota final= " + resultado + ". Reprovado." + " Faltam " + String.format("%.2f", (porcentagem/10 - resultado)) + " pontos";
+		if (resultado < porcentagem / 10) {
+			return "nota final= " + resultado + ". Reprovado." + " Faltam "
+					+ String.format("%.2f", (porcentagem / 10 - resultado)) + " pontos";
 		} else {
 			return "nota final= " + resultado + ". Aprovado.";
 		}
